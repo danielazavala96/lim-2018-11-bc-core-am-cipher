af351203df7fcbc5350f9c65112ecef498e4d644
danielazavala96 <daniela31zc@gmail.com>
2018-11-19 13:09:14 -0500

    Anadi boton en la pantalla

--- src/index.js
+++ src/index.js
@@ -7,3 +7,13 @@ document.getElementById("creador").style.display = "block";
 }
 //Asociar el evento a crear
 botonClick.addEventListener("click" , cipher_1);
+//llamar mensaje hacia otro input
+
+const resultMessage = document.getElementById("cifrar");
+const result = "";
+
+ function resultado_1() {
+   string = document.getElementById('cifrar').value;
+   resultMessage.innerHTML = result;
+
+ }
