7c00915cb6ae3216090da283d41d8982d7b6507f
danielazavala96 <daniela31zc@gmail.com>
2018-11-20 17:19:53 -0500

    cambios en js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-const botonClick = document.getElementById("crear");
+//const botonClick = document.getElementById("crear");
 
-function cipher_1(){
-document.getElementById("root").style.display = "none";
-document.getElementById("inicio").style.display = "none";
-document.getElementById("creador").style.display = "block";
-}
+////function cipher_1(){
+//document.getElementById("root").style.display = "none";
+//document.getElementById("inicio").style.display = "none";
+//document.getElementById("creador").style.display = "block";
+
 //Asociar el evento a crear
-botonClick.addEventListener("click" , cipher_1);
+//botonClick.addEventListener("click" , cipher_1);
 //llamar mensaje hacia otro input
 
-const resultMessage = document.getElementById("cifrar");
-const result = "";
 
- function resultado_1() {
-   string = document.getElementById('cifrar').value;
-   resultMessage.innerHTML = result;
+const buttonCipher = document.getElementById("btnEnviar_1");
+const textoInicial = document.getElementById("textcipher");
+const textoResult = document.getElementById("resultcipher");
 
- }
+const cifrado = () => {
+  const textoInicial = document.getElementById("textcipher").value;
+  return textoInicial;
+
+}
+buttonCipher.addEventListener("click", () => {
+
+  textoResult.value = cifrado();
+
+})
