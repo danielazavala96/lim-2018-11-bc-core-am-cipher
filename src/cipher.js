7cf418c3c856852ad5712e6290e4aeda167f50b7
danielazavala96 <daniela31zc@gmail.com>
2018-11-22 19:05:06 +0100

    Cifardo y Desscifrado listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,47 @@
-window.cipher = {
-  // ... 
-};
+//window.cipher = {
+  // ...
+//};
+const inputTextoInicial = document.getElementById("textcipher");
+const inputOffset = document.getElementById("offset");
+const textoResult = document.getElementById("resultcipher");
+const buttonCipher = document.getElementById("btnEnviar_1");
+
+const cifrado = (textoInicial, offset) => {
+  let textoFinal = "";
+  const textoMayus = textoInicial.toUpperCase();
+  for(var i=0; i<textoMayus.length; i++){
+    textoFinal+= String.fromCharCode((textoMayus.charCodeAt(i)-65+offset)%26+65);
+  }
+  return textoFinal;
+
+}
+
+buttonCipher.addEventListener("click", function(){
+  let offset = parseInt(inputOffset.value);
+  let textoInicial = inputTextoInicial.value;
+  textoResult.value = cifrado(textoInicial, offset);
+  console.log(cifrado(textoInicial, offset));
+})
+
+const inputTextoInicial2 = document.getElementById("textcipher_1");
+const inputOffset2 = document.getElementById("offset1");
+const textoResult2 = document.getElementById("resultcipher_1");
+const buttonCipher2 = document.getElementById("btnEnviar_2");
+
+const descifrado = (textoInicial2, offset1) => {
+  let textoFinal2 = "";
+  const newoffset1 = offset1%26;
+  const texto_Mayus = textoInicial2.toUpperCase();
+  for(var i=0; i<texto_Mayus.length; i++){
+    textoFinal2+= String.fromCharCode((texto_Mayus.charCodeAt(i)+65-newoffset1)%26+65);
+  }
+  return textoFinal2;
+
+}
+buttonCipher2.addEventListener("click", function(){
+  let offset1 = parseInt(inputOffset2.value);
+  let textoInicial2 = inputTextoInicial2.value;
+  textoResult2.value = descifrado(textoInicial2, offset1);
+  console.log(descifrado(textoInicial2, offset1));
+})
+//console.log(descifrado("beba", 1));
--- src/index.js
+++ src/index.js
@@ -1,26 +1,55 @@
-//const botonClick = document.getElementById("crear");
+//const botonClick = document.getElementById("emisor");
 
-////function cipher_1(){
+//function cipher_1(){
 //document.getElementById("root").style.display = "none";
-//document.getElementById("inicio").style.display = "none";
+//document.getElementById("invitado").style.display = "none";
 //document.getElementById("creador").style.display = "block";
-
+//}
 //Asociar el evento a crear
 //botonClick.addEventListener("click" , cipher_1);
 //llamar mensaje hacia otro input
-
-
-const buttonCipher = document.getElementById("btnEnviar_1");
-const textoInicial = document.getElementById("textcipher");
-const textoResult = document.getElementById("resultcipher");
-
-const cifrado = () => {
-  const textoInicial = document.getElementById("textcipher").value;
-  return textoInicial;
-
+const inicioPantalla = () => {
+  document.getElementById("creador").style.display = "none";
+  document.getElementById("root").style.display = "block";
+  document.getElementById("invitado").style.display = "none";
 }
-buttonCipher.addEventListener("click", () => {
+inicioPantalla();
 
-  textoResult.value = cifrado();
+const botonCrea = document.getElementById("emisor");
+const pantallaNumb1 = () => {
+  document.getElementById("creador").style.display = "block";
+  document.getElementById("root").style.display = "none";
+  document.getElementById("invitado").style.display = "none";
+}
+botonCrea.addEventListener("click",pantallaNumb1);
 
-})
+const botonBack = document.getElementById("back1")
+const volverInicio1 = () => {
+  document.getElementById("root").style.display = "block";
+  document.getElementById("creador").style.display = "none";
+  document.getElementById("invitado").style.display = "none";
+}
+botonBack.addEventListener("click",volverInicio1);
+
+const botonDescubre = document.getElementById("receptor")
+const pantallaNumb2 = () => {
+  document.getElementById("invitado").style.display = "block";
+  document.getElementById("root").style.display = "none";
+  document.getElementById("creador").style.display = "none";
+}
+botonDescubre.addEventListener("click",pantallaNumb2);
+
+const botonBack2 = document.getElementById("back2")
+const volverInicio2 = () => {
+  document.getElementById("root").style.display = "block";
+  document.getElementById("invitado").style.display = "none";
+  document.getElementById("creador").style.display = "none";
+}
+botonBack2.addEventListener("click",volverInicio2);
+
+//let botonclean = document.getElementById("borrar")
+//let borrarCampo = document.getElementById("resultcipher_1")
+//function cleanField(){
+//    borrarCampo.value = "";
+//}
+//botonclean.addEventListener("click", cleanField);
